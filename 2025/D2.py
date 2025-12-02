9bc26a516a6fed16d60452580da70e49879c3385
Andrea Pinardi <andreapinardi319@gmail.com>
2025-12-02 10:13:20 +0100

    Add day 2, puzzle 2

--- 2025/D2.py
+++ 2025/D2.py
@@ -24,28 +24,15 @@ ranges = data.split(',')
 # split each string '11-22' to ['11', '22'], the apply int() to convert
 ranges = [list(map(int, range_i.split('-'))) for range_i in ranges]
 invalid_ID = []
+
+
+#%% PUZZLE 1
+
 for range_i in ranges:
     # list of IDs in between the 2 limits
     # (+1 to include last value)
     IDs = list(map(str, range(range_i[0], range_i[1]+1)))
     for ID in IDs:
-# =============================================================================
-#         # a string can split in equal pieces of N different lengths, where the 
-#         # lengths are the divisors of the number
-#         # (exclude 1)
-#         div = list(divisors(len(ID)))[1:]
-#         for N in div:
-#             # split string into pieces of length len(ID)/N
-#             # https://stackoverflow.com/a/9477447/17220538
-#             L = int(len(ID)/N)
-#             pieces = findall('.'*L, ID)
-#             # pick the non-repeating pieces converting to a set: if now len = 1,
-#             # then there are only repeated digits => invalid ID
-#             uniques = set(pieces)
-#             if len(uniques) == 1:
-#                 invalid_ID.append(ID)
-#                 break
-# =============================================================================
         # if you can't split the string in groups of digits repeated twice,
         # then the ID won't be invalid
         if len(ID) % 2 != 0:
@@ -62,4 +49,32 @@ for range_i in ranges:
             invalid_ID.append(ID)
 
 sum_invalid = np.sum(np.array(list(map(int, invalid_ID))))
+print(f'Sum of invalid IDs: {sum_invalid}')
+
+
+#%% PUZZLE 2
+
+all_invalid_ID = []
+for range_i in ranges:
+    # list of IDs in between the 2 limits
+    # (+1 to include last value)
+    IDs = list(map(str, range(range_i[0], range_i[1]+1)))
+    for ID in IDs:
+        # a string can split in equal pieces of N different lengths, where the 
+        # lengths are the divisors of the number
+        # (exclude 1)
+        div = list(divisors(len(ID)))[1:]
+        for N in div:
+            # split string into pieces of length len(ID)/N
+            # https://stackoverflow.com/a/9477447/17220538
+            L = int(len(ID)/N)
+            pieces = findall('.'*L, ID)
+            # pick the non-repeating pieces converting to a set: if now len = 1,
+            # then there are only repeated digits => invalid ID
+            uniques = set(pieces)
+            if len(uniques) == 1:
+                all_invalid_ID.append(ID)
+                break
+
+sum_invalid = np.sum(np.array(list(map(int, all_invalid_ID))))
 print(f'Sum of invalid IDs: {sum_invalid}')
