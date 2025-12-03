54ddf370a4ac6ab3a667f43f801e5d844d6062fd
Andrea Pinardi <andreapinardi319@gmail.com>
2025-12-03 14:33:20 +0100

    Add day 3, puzzle 2

--- 2025/D3.py
+++ 2025/D3.py
@@ -23,6 +23,9 @@ for bank in data:
     temp = [int(jolt) for jolt in bank]
     banks.append(np.array(temp))
 
+
+#%% PUZZLE 1
+
 jolts = []
 for bank in banks:
     idx_max = np.argmax(bank)
@@ -45,3 +48,44 @@ jolts = np.array(jolts)
 tot_jolt = np.sum(jolts)
 
 print(f'Total joltage output: {tot_jolt}') 
+
+
+#%% PUZZLE 2
+
+jolts = []
+for bank in banks:
+    bank_temp = bank.copy()
+    # find max value and check whether there are at least other 11 digits
+    # after it to create a 12-digits number
+    # set to false to start the loop
+    enough_digits = False
+    
+    jolt = []
+    for n_digit in range(12):
+        digit_found = False
+        bank_temp2 = bank_temp.copy()
+        while not digit_found:
+            max_num = np.max(bank_temp2)
+            idx_max = np.argmax(bank_temp2)
+            N_digits_dx = bank_temp.shape[0] - (idx_max + 1)
+            # check whether there are enough digits to the right of the max to 
+            # create a new joltage
+            if N_digits_dx < (12-(n_digit+1)):
+                # temporary remove the max value you've found
+                bank_temp2[idx_max] = -1
+                digit_found = False
+            else:
+                digit_found = True
+        jolt.append(str(max_num))
+        # replace all values to the left of the max (and the max) with dummies
+        bank_temp[0:idx_max+1] = -1
+    # from ['2', '3', '4'] to '234'
+    jolt = int(''.join(jolt))
+    jolts.append(jolt)
+
+jolts = np.array(jolts)
+tot_jolt = np.sum(jolts)
+
+print(f'Total joltage output: {tot_jolt}')
+
+# 3121910778619 => too low
