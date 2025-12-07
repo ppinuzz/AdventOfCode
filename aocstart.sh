0bfeaba4542ff7aa77df5ae1cad0832669cf2bdc
Andrea Pinardi <andreapinardi319@gmail.com>
2025-12-07 01:14:05 +0100

    Add option to specify year folder

--- aocstart.sh
+++ aocstart.sh
@@ -1,5 +1,33 @@
 #!/usr/bin/bash
 
+# check whether the user has provided a -y or --year argument
+year=""
+# repeat until the number of positional arguments passed to the shell ($#) is > 0
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -y|--year)
+            year="$2"
+			# once you have the value, remove the flag -y and the year number (i.e. shifts the argument list of 2 positions)
+            shift 2
+            ;;
+		-h|--help)
+			# to ensure proper formatting and alignment, use a heredoc with <<EOF (see below)
+            cat<<EOF
+Usage: aocstart.sh [options]
+
+Options:
+	-y, --year	specify the year folder (default: current year)
+	-h, --help	print this string
+EOF
+            exit 0
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
+
 # get today's day number
 # 1) date +%d	outputs day of the mont with 2 digits (e.g. 03)
 # 2) pass to sed to remove leading 0, if present
@@ -8,7 +36,10 @@
 #	0*	any number of 0s
 #	since the 2nd /.../ is empty, this replaces all the leading 0s with nothing
 day_number=$(date +%d | sed 's/^0*//')
-year=$(date +%Y)
+# if user didn't specify -y (and thus 'year' has length 0), use current year
+if [[ -z "$year" ]]; then
+    year=$(date +%Y)
+fi
 # full date string like 'Mon Dec  1 09:20:22 2025'
 today=$(date +"%a %b %e %H:%M:%S %Y")
 
